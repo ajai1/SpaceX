8243c158e685ceef8ff8a5f0455c4a80c7e85491
Ajai Kumar Mohan <ajaikumarmohan@PMLBLRMB4000549.local>
2021-05-28 14:00:13 +0530

    Launches Info

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import Ship from "./Ship/Ship"
 import Rocket from "./Rockets/Rocket"
+import Launches from './Launches/Launches'
 
 
 const Dashboard = () => {
     return (
         <div>
             <h2>Dashboard</h2>
-           <Rocket/>
-            <Ship/> 
+            <Launches/>
         </div>
     )
 }
--- src/Components/Dashboard/Launches/Launches.js
+++ src/Components/Dashboard/Launches/Launches.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+import LaunchInfo from "./LaunchInfo"
 import "./Launches.css";
 
 function Launches() {
   const [launches, setLaunches] = useState([]);
+  const [launch, setLaunch] = useState();
 
   useEffect(() => {
     async function getData() {
       let res = await fetch("https://api.spacexdata.com/v3/launches");
       let data = await res.json();
       setLaunches(data);
+      setLaunch(data[2]);
     }
-
     getData();
   }, []);
 
-  return <div></div>;
+
+  return <div className="launches_container">
+   {
+      launch && <LaunchInfo launch={launch}/>
+    } 
+  </div>;
 }
 
 export default Launches;
